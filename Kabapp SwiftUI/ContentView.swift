36a9d67e700b00d6e9b5d3ccc8a51b86fc15501d
ChrisKabbouchy <57867758+ChrisKabbouchy@users.noreply.github.com>
2019-12-29 21:40:30 +0200

    UI created with SwiftUI

--- Kabapp SwiftUI/ContentView.swift
+++ Kabapp SwiftUI/ContentView.swift
@@ -10,12 +10,103 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            
+            topView()
+            Spacer()
+            imageView()
+            Spacer()
+            bottomView()
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct topView: View {
+    var body: some View {
+        HStack{
+            Button(action: {print("hello")}) {
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .frame(width: 45, height: 45)
+                    .foregroundColor(.gray)
+                    .aspectRatio(contentMode: .fit)
+            }
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image(systemName: "flame.fill")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.red)
+                    .aspectRatio(contentMode: .fit)
+            }
+            
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image(systemName: "message.fill")
+                    .resizable()
+                    .frame(width: 45, height: 45)
+                    .foregroundColor(.gray)
+                    .aspectRatio(contentMode: .fit)
+            }
+            
+            
+        }.padding()
+    }
+}
+
+struct bottomView: View {
+    var body: some View {
+        HStack{
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image(systemName: "arrowshape.turn.up.right.circle.fill")
+                    .resizable()
+                    .frame(width: 45, height: 45)
+                    .foregroundColor(.yellow)
+                    .aspectRatio(contentMode: .fit)
+            }
+            Spacer()
+            Button(action: {}) {
+                Image(systemName: "multiply.circle.fill")
+                    .resizable()
+                    .frame(width: 45, height: 45)
+                    .foregroundColor(.red)
+                    .aspectRatio(contentMode: .fit)
+            }
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .frame(width: 45, height: 45)
+                    .foregroundColor(.blue)
+                    .aspectRatio(contentMode: .fit)
+            }
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image(systemName: "heart.fill")
+                    .resizable()
+                    .frame(width: 45, height: 45)
+                    .foregroundColor(.red)
+                    .aspectRatio(contentMode: .fit)
+            }
+            Spacer()
+        }.padding()
+    }
+}
+
+struct imageView: View {
+    var body: some View {
+        Image("image")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .cornerRadius(30)
+            .padding(.horizontal)
     }
 }
